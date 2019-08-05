af76b31f367d4199eef7d5ddfce337ca34d8361a
Jay Swanson <jay.swanson@P15528.local>
2019-08-05 17:42:08 -0600

    similar cam logic file added and imported(CamManip.js)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import CamObjects from './CamObjects';
 import ParentCompare from './ParentCompare';
+import CamManip from './CamManip';
 
 function App() {
   return (
